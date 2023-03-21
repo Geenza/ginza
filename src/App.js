fe60932b497256c2c210a186d532a371fd03d239
Geenza <sherzamon@tuit.uz>
2023-03-21 04:56:48 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Home from './src/component/home';
+
+function App() {
+  return (
+    <div className="App">
+    <Home/>
+
+    </div>
+  );
+}
+
+export default App;
