6f1dd0e6f056169e09dac4286f09f691f8bea376
Geenza <sherzamon@tuit.uz>
2023-03-21 14:45:33 -0700

    sher

--- src/component/homepage/homepage.js
+++ src/component/homepage/homepage.js
@@ -1,8 +1,10 @@
 import React from "react";
 import './style.css'
 import logo from './icons8-bash-480.svg'
-import img from './img/book.png'
+import img from './img/pc.jpg'
 import wall from './img/wall.jpg'
+import robot from './img/robot.png'
+
 
 
 function Homepage() {
@@ -74,11 +76,193 @@ function Homepage() {
                         <h2>Chuqurlashtirilgan kurslar</h2>
                     </div>
                     <div className={'card'}>
-                        <h2>Maxsus praktikum kurslar</h2>
+                        <h2 className={'btn-4'}>Maxsus praktikum kurslar</h2>
                     </div>
                 </div>
             </div>
         </div>
+        <div className="about">
+            <div className="about-content">
+                <div className={'about-text'}>
+                    <p>Ginza jamoasi </p>
+                    <h2>Nima uchun Ginza da o'qish kerak? </h2>
+                    <button>Videoni ko'rish</button>
+                    <div/>
+                </div>
+                <div className={'about-video'}>
+                    <iframe/>
+                </div>
+
+            </div>
+        </div>
+        <div className={'jobs'}>
+            <div className={'jobs-content'}>
+                <div className={'jobs-text'}>
+                    <h1>Kasblar</h1>
+                    <p>Kasbga yo'nalitirilgan praktikumlar yordamida eng tez va samarali yo'llar bilan mutaxassislar qatoriga qo'shiling. Har bir praktikum soha mutaxassislari tomonidan eng zamoaviy o'quv reja asosida tayyorlangan.</p>
+
+                </div>
+                <div className={'jobs-type'}>
+                    <button>Frontend</button>
+                    <button>Backend</button>
+                    <button>Fullstack</button>
+                    <button>Mobile</button>
+                    <button>UI/UX</button>
+                    <button>Game</button>
+                    <button>DevOps</button>
+                    <button>QA</button>
+                    <button>Project</button>
+                    <button>Marketing</button>
+                    <button>Business</button>
+                    <button>Other</button>
+                </div>
+                <div className={'jobs-cards'}>
+                    <div className={'job-card'}>
+                        <h2>Front end</h2>
+                    </div>
+                 <div className={'job-card'}>
+                        <h2>Front end</h2>
+                    </div>
+                 <div className={'job-card'}>
+                        <h2>Front end</h2>
+                    </div>
+                 <div className={'job-card'}>
+                        <h2>Front end</h2>
+                    </div>
+                 <div className={'job-card'}>
+                        <h2>Front end</h2>
+                    </div>
+                  <div className={'job-card'}>
+                        <h2>Front end</h2>
+                    </div>
+                  <div className={'job-card'}>
+                        <h2>Front end</h2>
+                    </div>
+                  <div className={'job-card'}>
+                        <h2>Front end</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className={'kurslar'}>
+            <h1>Kurslar</h1>
+            <div className={'kurslar-type'}>
+                <button>Bepul kurslar</button>
+                <button>Backend</button>
+                <button>Frontend</button>
+                <button>Fullstack</button>
+                <button>Mobile</button>
+
+            </div>
+            <div className={'kurslar-cards'}>
+                <div className={'kurslar-card'}>
+                    <img src={robot} alt=""/>
+                    <p1>Muhiddinov Sherzamon</p1>
+                    <p2>300 o'quvchi</p2>
+                    <h2>Django 3 -pythonda Full stack Web dasturlasjh</h2>
+                    <p3>Describe course, Ma’lumotlaringizni qoldiring va sizga tez fursatda aloqaga chiqamiz</p3>
+                    <p4>250 000 so'm</p4>
+
+                    <button>Kirish</button>
+
+                </div>
+                <div className={'kurslar-card'}>
+                    <img src={robot} alt=""/>
+                    <p1>Muhiddinov Sherzamon</p1>
+                    <p2>300 o'quvchi</p2>
+                    <h2>Django 3 -pythonda Full stack Web dasturlasjh</h2>
+                    <p3>Describe course, Ma’lumotlaringizni qoldiring va sizga tez fursatda aloqaga chiqamiz</p3>
+                    <p4>250 000 so'm</p4>
+
+                    <button>Kirish</button>
+
+                </div>
+            <div className={'kurslar-card'}>
+                    <img src={robot} alt=""/>
+                    <p1>Muhiddinov Sherzamon</p1>
+                    <p2>300 o'quvchi</p2>
+                    <h2>Django 3 -pythonda Full stack Web dasturlasjh</h2>
+                    <p3>Describe course, Ma’lumotlaringizni qoldiring va sizga tez fursatda aloqaga chiqamiz</p3>
+                    <p4>250 000 so'm</p4>
+
+                    <button>Kirish</button>
+
+                </div>
+            <div className={'kurslar-card'}>
+                    <img src={robot} alt=""/>
+                    <p1>Muhiddinov Sherzamon</p1>
+                    <p2>300 o'quvchi</p2>
+                    <h2>Django 3 -pythonda Full stack Web dasturlasjh</h2>
+                    <p3>Describe course, Ma’lumotlaringizni qoldiring va sizga tez fursatda aloqaga chiqamiz</p3>
+                    <p4>250 000 so'm</p4>
+
+                    <button>Kirish</button>
+
+                </div>
+            <div className={'kurslar-card'}>
+                    <img src={robot} alt=""/>
+                    <p1>Muhiddinov Sherzamon</p1>
+                    <p2>300 o'quvchi</p2>
+                    <h2>Django 3 -pythonda Full stack Web dasturlasjh</h2>
+                    <p3>Describe course, Ma’lumotlaringizni qoldiring va sizga tez fursatda aloqaga chiqamiz</p3>
+                    <p4>250 000 so'm</p4>
+
+                    <button>Kirish</button>
+
+                </div>
+                <div className={'kurslar-card'}>
+                    <img src={robot} alt=""/>
+                    <p1>Muhiddinov Sherzamon</p1>
+                    <p2>300 o'quvchi</p2>
+                    <h2>Django 3 -pythonda Full stack Web dasturlasjh</h2>
+                    <p3>Describe course, Ma’lumotlaringizni qoldiring va sizga tez fursatda aloqaga chiqamiz</p3>
+                    <p4>250 000 so'm</p4>
+
+                    <button>Kirish</button>
+
+                </div>
+                <div className={'kurslar-card'}>
+                    <img src={robot} alt=""/>
+                    <p1>Muhiddinov Sherzamon</p1>
+                    <p2>300 o'quvchi</p2>
+                    <h2>Django 3 -pythonda Full stack Web dasturlasjh</h2>
+                    <p3>Describe course, Ma’lumotlaringizni qoldiring va sizga tez fursatda aloqaga chiqamiz</p3>
+                    <p4>250 000 so'm</p4>
+
+                    <button>Kirish</button>
+
+                </div>
+                <div className={'kurslar-card'}>
+                    <img src={robot} alt=""/>
+                    <p1>Muhiddinov Sherzamon</p1>
+                    <p2>300 o'quvchi</p2>
+                    <h2>Django 3 -pythonda Full stack Web dasturlasjh</h2>
+                    <p3>Describe course, Ma’lumotlaringizni qoldiring va sizga tez fursatda aloqaga chiqamiz</p3>
+                    <p4>250 000 so'm</p4>
+
+                    <button>Kirish</button>
+
+                </div>
+            </div>
+
+
+
+        </div>
+        <div className={'contact-form'}>
+            <h1>Ma'lumotlaringizni qoldiring va sizga tez fursatda aloqaga chiqamiz</h1>
+            <div className={'contact-form-input'}>
+                <input type="text" placeholder={'Ismingiz'}/>
+                <input type="text" placeholder={'Telefon raqamingiz'}/>
+                <input type="text" placeholder={'Emailingiz'}/>
+                <input type="text" placeholder={'Kurs nomi'}/>
+                <input type="text" placeholder={'Kurslar nomi'}/>
+                <button>Jo'natish</button>
+
+        </div>
+        </div>
+
+
+
 
 
 
